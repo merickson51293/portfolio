bf4b21d26191442e1ed73ad21062d6dfa6a36efc
Michael.Erickson <merickson51293@gmail.com>
2021-01-06 09:35:34 -0500

    added blog post page

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('', views.index),
     path('blog', views.blog),
+    path('blog_post/<int:blog_id>', views.blog_post)
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    context={
+        "blogs":Blog.objects.all()
+    }
+    return render(request, "index.html", context)
 
 def blog(request):
-    return render(request, "blog.html")
+    context={
+        "blogs":Blog.objects.all()
+    }
+    return render(request, "blog.html", context)
+
+def blog_post(request, blog_id):
+    context={
+        "blogs":Blog.objects.get(id=blog_id)
+    }
+    return render(request, "blog_post.html", context)
 
     
