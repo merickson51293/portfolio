290a7cc788a75c3945b57d6f876e9412f176d1f6
Michael.Erickson <merickson51293@gmail.com>
2021-01-06 08:42:41 -0500

    added blog

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.title}"
